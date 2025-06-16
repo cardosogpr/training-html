5b02c144d12bc7b0272f056da2f9d2a75d24bd69
cardosogpr <rui.pedro.goncalves.cardoso@gmail.com>
2025-06-16 18:25:14 +0100

    function value console

---
+++ 05 Exercício/main.js
@@ -0,0 +1,35 @@
+// class User {
+//     constructor(email, address, name, number, password, gender, club) {
+//         this.email = email;
+//         this.address = address;
+//         this.club = club;
+//     }
+
+//     show() {
+//         console.log(this)
+//     }
+// }
+// // let newUser = {
+
+// //     email: document.getElementById("inputEmail").value,
+// //     address: document.getElementById("inputAddress").value,
+// //     club: document.getElementById("inputClube").value
+// // };
+
+
+// function getValuesFromForm() {
+//     let newUserByClass = new User(
+
+//         document.getElementById("inputEmail").value,
+//         document.getElementById("inputAdd").value,
+//         document.getElementById("inputClube").value
+
+//     );
+//     newUserByClass.show()
+// }
+
+
+
+// // newUser.email = document.getElementById("inputEmail").value;
+// // newUser.address = document.getElementById("inputAddress").value;
+// // newUser.club = document.getElementById("inputClube").value;
